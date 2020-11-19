cc9855ed7ed39cbd5c80d988e4814a49ed81c9df
bytedance <shaoyufei@bytedance.com>
2020-11-19 18:40:22 +0800

    # 打造自己的框架第一次作业提交

--- myframe/test_demo.py
+++ myframe/test_demo.py
@@ -5,7 +5,6 @@ import time
 import pytest
 import yaml
 from selenium import webdriver
-from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.remote.webelement import WebElement
 
 
@@ -17,27 +16,26 @@ def load_data(path):
 
 class TestDemo:
 
+    @classmethod
+    def setup_class(cls):
+        cls.driver = webdriver.Chrome()
+        pass
+
     # 参数化数据
     @pytest.mark.parametrize('data', load_data("test_data.yaml")['data'])
     def test1(self, data):
 
         for step in load_data("test_data.yaml")['steps']:
             print(step)
-            if "driver" in step:
-                browser = str(step.get("driver").get("browser")).lower()
-                if browser == "chrome":
-                    driver: WebDriver = webdriver.Chrome()
-                else:
-                    print(f"{driver} don't know which browser")
 
-            elif "get" in step:
+            if "get" in step:
                 url = step.get("get")
-                driver.get(url)
+                self.driver.get(url)
 
             elif 'find_element' in step:
                 by = step.get("find_element")['by']
                 locator = step.get("find_element")['value']
-                current_element: WebElement = driver.find_element(by, locator)
+                current_element: WebElement = self.driver.find_element(by, locator)
 
             elif "click" in step:
                 current_element.click()
@@ -49,3 +47,26 @@ class TestDemo:
 
             elif 'sleep' in step:
                 time.sleep(step.get('step'))
+
+    # 自定义
+    @pytest.mark.parametrize('data', load_data("test_data.yaml")['data'])
+    def test2_my_frame(self, data):
+        for step in load_data("test_data.yaml")['steplists']:
+            # 获取动作
+            action = [key for key in step.keys()][0]
+            # 获取参数
+            values = step.get(action) if isinstance(step.get(action), list) else [step.get(action)]
+            common = ''
+            for value in values:
+                # 判断数据驱动
+                if '${data}' in value:
+                    value = data
+                # 拼接参数
+                common = common + '\'{}\', '.format(value)
+            if action in ['click', 'send_keys']:
+                # 拼接执行语句
+                pd = 'current_element.{}({})'.format(action, common)
+            else:
+                pd = 'self.driver.{}({})'.format(action, common)
+            # 执行语句并获取返回值
+            current_element = eval(pd)
